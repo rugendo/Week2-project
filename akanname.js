cda0be87cc1e03b9f2df5a6fe85624b871794c4e
rugendo <ercrugendo@gmail.com>
2022-11-01 04:35:59 +0300

    javascript functions created on js file

--- akanname.js
+++ akanname.js
@@ -1,4 +1,31 @@
 //console.log("get the akan name")
 
-const maleNames = ("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame")
+//get the birthday input
+//get the gender input
+//after submitting the information selected, get alert with the Akan name
 
+   function getElements() {
+     const dateElements = document.getElementById("birthday");
+     const genderElements = document.getElementById("gender");
+    
+   }
+
+   function getInputs() {
+    const dateElements = input.dateElements;
+    const genderElements = input.genderElements;
+
+    const dateValue = dateElements.value;
+    const genderValue =genderElements.value;
+
+   }
+
+function submitFunction() {
+    alert(femaleNames)
+}
+
+
+
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+
+
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"] 
