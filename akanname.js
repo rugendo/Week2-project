4236dfd5101e32acca3afcc55c8c78cba5c68d84
rugendo <ercrugendo@gmail.com>
2022-11-22 13:54:26 +0300

    created a fucntion that gives the user their Akan name

--- akanname.js
+++ akanname.js
@@ -3,32 +3,38 @@
 //get the birthday input
 //get the gender input
 //after submitting the information selected, get alert with the Akan name
-const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+//const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 
-const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"] 
+//const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"] 
 
-
-   function dateInput() {
-    let bDate = document.getElementById("birthday").innerHTML;  
-     
     
-   }
-
-   function genderInput() {
-    let genderValue = document.getElementById("gender").innerHTML;
-    if (genderValue = Male) {
-      return (maleNames);
-      
-    } else {
-      return (femaleNames);
-      
+  function getInput() {
+    //get the date and gender inputs from the user
+    let genderValue = document.getElementById("gender").value;
+    let getDateOfBirth = document.getElementById("birthday").value;  
+    
+    if (getDateOfBirth == ""){
+      alert("Select Date of Birth");
     }
-  
-   }
+    //get the day of week
+    let date = new Date(getDateOfBirth);
+    let eBirthday = date.getDay();    
+    //the Akan name Arrays
+    const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-function submitFunction() {
-  alert(maleNames)
-}
+    if (genderValue == ("Female") && getDateOfBirth != ""){
+      alert("Your Akan name is" + " " + femaleNames[eBirthday]);
+    }
+    else if (genderValue == ("Male")  && getDateOfBirth != ""){
+      alert("Your Akan name is" + " " + maleNames[eBirthday]);
+    }
+    
+  }
+
+getInput();
+
+
 
 
 
